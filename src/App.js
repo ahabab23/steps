dcc24098a073b5de193093bf1b588a92e9f73da2
Uwes Ahabab <uwesahabab@gmail.com>
2024-05-19 15:23:50 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
 function App() {
+  const [steps, setSteps] = useState(1);
+  const [display, setDisplay] = useState(true);
+
+  function NextHandler() {
+    if (steps < messages.length) {
+      setSteps((step) => step + 1);
+    }
+  }
+  function PrevHandler() {
+    if (steps > 1) {
+      setSteps((step) => step - 1);
+    }
+  }
+  function DisplayHandler() {
+    setDisplay((display) => !display);
+  }
+
+  return display ? (
+    <div className="steps">
+      <Numbers step={steps} />
+      <Message step={steps} />
+      <Buttons NextHandler={NextHandler} PrevHandler={PrevHandler} />
+      <button className="close" onClick={DisplayHandler}>
+        &times;
+      </button>
+    </div>
+  ) : (
+    <button className="close" onClick={DisplayHandler}>
+      &times;
+    </button>
+  );
+}
+function Numbers({ step }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="numbers">
+      <div className={step >= 1 ? "active" : ""}>1</div>
+      <div className={step >= 2 ? "active" : ""}>2</div>
+      <div className={step >= 3 ? "active" : ""}>3</div>
     </div>
   );
 }
 
+function Message({ step }) {
+  return (
+    <div className="message">
+      <h3>steps:{step}</h3>
+      <p>{messages[step - 1]}</p>
+    </div>
+  );
+}
+function Buttons({ NextHandler, PrevHandler }) {
+  const colorstyle = { color: "#fff", backgroundColor: "#7950f2" };
+  return (
+    <div className="buttons">
+      <button style={colorstyle} onClick={PrevHandler}>
+        Previous
+      </button>
+      <button style={colorstyle} onClick={NextHandler}>
+        Next
+      </button>
+    </div>
+  );
+}
 export default App;
